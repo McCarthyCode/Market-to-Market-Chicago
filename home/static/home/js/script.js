d16240c36ee1571554a007ad305c2c9e29b65a87
Matt McCarthy <mattmc318@gmail.com>
2019-10-29 02:57:49 -0500

    Initial commit with home page layout

---
+++ home/static/home/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  console.log('This site brought to you by McCarthy Web Design.');
+  console.log('https://mccarthywebdesign.com/');
+
+  $('#navbarMenuButton').click(() => {
+    $('#navbarCollapse').slideToggle();
+  });
+});
