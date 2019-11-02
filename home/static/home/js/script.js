a7a2c5b703415bdde23a8ee0e9c2a5abc50cba1d
Matt McCarthy <mattmc318@gmail.com>
2019-11-01 20:19:46 -0500

    Added external link icons

--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -2,6 +2,9 @@ $(document).ready(() => {
   console.log('This site brought to you by McCarthy Web Design.');
   console.log('https://mccarthywebdesign.com/');
 
+  $('a.external-link')
+    .after(' <i class="fas fa-external-link-alt" title="External Link"></i>');
+
   let breakpointMd = 768;
 
   $('#navbarMenuButton').click(() => {
