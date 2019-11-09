f61b03097b66e96a024fc4b2cfef798600e88eb6
Matt McCarthy <mattmc318@gmail.com>
2019-11-08 21:14:49 -0600

    Created front-end for user login; updated MWD link in console

--- home/static/home/js/global.js
+++ home/static/home/js/global.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   console.log('This site brought to you by McCarthy Web Design.');
-  console.log('https://mccarthywebdesign.com/');
+  console.log('https://mccarthy-web-design.000webhostapp.com/');
 
   $('a.external-link')
     .after(' <i class="fas fa-external-link-alt" title="External Link"></i>');
