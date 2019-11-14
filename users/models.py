1bcee8c270ffa91e6f211c8689c6cd06f9d29a72
Matt McCarthy <mattmc318@gmail.com>
2019-11-13 21:04:09 -0600

    Added Author class to models

--- users/models.py
+++ users/models.py
@@ -5,3 +5,7 @@ from users import managers
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     objects = managers.ProfileManager()
+
+class Author(Profile):
+    profile_pic = models.ImageField(upload_to='media/img/')
+    bio = models.CharField(max_length=500)
