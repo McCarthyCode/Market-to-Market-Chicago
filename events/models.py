36c6f54ceedd5c873c9bc7aa5e51afdb35cb87b2
Matt McCarthy <mattmc318@gmail.com>
2019-11-21 21:37:43 -0600

    Defined Events app models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from home.models import TimestampedModel
 
-# Create your models here.
+class Location(TimestampedModel):
+    name = models.CharField(max_length=200)
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(null=True, blank=True, max_length=200)
+    city = models.CharField(max_length=80, default='Chicago')
+    state = models.CharField(max_length=2, default='IL')
+    zip = models.CharField(null=True, blank=True, max_length=5)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(TimestampedModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField(null=True, blank=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
