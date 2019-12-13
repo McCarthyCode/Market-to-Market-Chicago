07f31feb8273837ee3a8404d9aadf4e0ebd99ea6
Matt McCarthy <mattmc318@gmail.com>
2019-12-13 01:58:56 -0600

    Moved JS compression from daemons.sh to compress_js.sh

--- bin/daemons.sh
+++ bin/daemons.sh
@@ -12,20 +12,6 @@ sass_input_users="users/static/users/sass/base.sass"
 sass_output_users="users/static/users/css/users.css"
 sass_output_users_compressed="users/static/users/css/users.min.css"
 
-closure_compiler="/opt/closure-compiler/closure-compiler-v20190929.jar"
-
-js_input_global="home/static/home/js/global.js"
-js_output_global="home/static/home/js/global.min.js"
-
-js_input_home="home/static/home/js/home.js"
-js_output_home="home/static/home/js/home.min.js"
-
-js_input_events="events/static/events/js/events.js"
-js_output_events="events/static/events/js/events.min.js"
-
-js_input_users="users/static/users/js/users.js"
-js_output_users="users/static/users/js/users.min.js"
-
 declare -a foo=()
 declare -a daemons=(
   "source env/bin/activate; python manage.py runserver 10.0.0.100:8000"
@@ -35,10 +21,6 @@ declare -a daemons=(
   "sass --watch --style=compressed $sass_input_events:$sass_output_events_compressed"
   "sass --watch $sass_input_users:$sass_output_users"
   "sass --watch --style=compressed $sass_input_users:$sass_output_users_compressed"
-  "watch java -jar $closure_compiler --js $js_input_global --js_output_file $js_output_global"
-  "watch java -jar $closure_compiler --js $js_input_home --js_output_file $js_output_home"
-  "watch java -jar $closure_compiler --js $js_input_events --js_output_file $js_output_events"
-  "watch java -jar $closure_compiler --js $js_input_users --js_output_file $js_output_users"
 )
 
 for i in "${daemons[@]}"; do
