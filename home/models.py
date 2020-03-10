248baa8c6edbc1defd6d667513456d66f4208260
Matt McCarthy <mattmc318@gmail.com>
2020-03-10 18:34:09 -0500

    Added thumbnail to Person model; updated person_home.html

--- home/models.py
+++ home/models.py
@@ -1,4 +1,6 @@
 import hashlib
+import os
+import re
 
 from base64 import b16encode
 from functools import partial
@@ -34,6 +36,8 @@ class Person(NewsItem):
     suffix = models.CharField(blank=True, null=True, max_length=5)
     image = models.ImageField(default=None, upload_to='people/')
     _image_hash = models.BinaryField(blank=True, null=True, default=None, max_length=16)
+    thumbnail = models.ImageField(editable=False, null=True, default=None, upload_to='people/')
+    _thumbnail_hash = models.BinaryField(blank=True, null=True, default=None, max_length=16)
     bio = models.TextField()
     phone = models.CharField(blank=True, null=True, max_length=10)
     email = models.EmailField(blank=True, null=True)
@@ -43,6 +47,8 @@ class Person(NewsItem):
 
     def save(self, *args, **kwargs):
         if self.image:
+            self.generate_thumbnail()
+            self.hash_thumbnail()
             self.resize_image()
             self.hash_image()
 
@@ -50,21 +56,52 @@ class Person(NewsItem):
 
         super().save(*args, **kwargs)
 
-    def hash_image(self, block_size=65536):
-        hasher = hashlib.md5()
+    def generate_thumbnail(self):
+        img = Image.open(self.image).convert('RGB')
+        width, height = img.size
+        max_longest, max_shortest = 300, 250
 
-        with open(MEDIA_ROOT + '/' + self.image.name, 'rb') as f:
+        if not self.thumbnail and (width >= height and (width > max_longest or height > max_shortest)) or (height > width and (height > max_longest or width > max_shortest)):
+            if width > height:
+                if (height * max_longest/ width) > max_shortest:
+                    new_height = max_shortest
+                    new_width = int(width * new_height / height)
+                else:
+                    new_width = max_longest
+                    new_height = int(height * new_width / width)
+            else:
+                if (width * max_longest / height) > max_shortest:
+                    new_width = max_shortest
+                    new_height = int(height * new_width / width)
+                else:
+                    new_height = max_longest
+                    new_width = int(width * new_height / height)
+
+            img = img.resize((new_width, new_height), Image.ANTIALIAS)
+            img_file = BytesIO()
+            img.save(img_file, 'JPEG', quality=90)
+
+            new_name = 'thumbnail_' + self.image.name.split('.')[0] + '.jpg'
+            self.thumbnail.save(new_name, img_file)
+
+    def hash_thumbnail(self, block_size=65536):
+        hasher = hashlib.md5()
+        filename = MEDIA_ROOT + '/' + self.thumbnail.name
+
+        with open(filename, 'rb') as f:
             for buf in iter(partial(f.read, block_size), b''):
                 hasher.update(buf)
 
-            if not self.image_hash or self.image_hash != hasher.hexdigest().lower():
-                self._image_hash = hasher.digest()
-                self.image.save(hasher.hexdigest().lower() + '.jpg', f)
+            if not self.thumbnail_hash or self.thumbnail_hash != hasher.hexdigest().lower():
+                self._thumbnail_hash = hasher.digest()
+                self.thumbnail.name = 'people/' + hasher.hexdigest().lower() + '.jpg'
+                new_filename = MEDIA_ROOT + '/' + self.thumbnail.name
+                os.rename(filename, new_filename)
 
     def resize_image(self):
         img = Image.open(self.image).convert('RGB')
         width, height = img.size
-        max_longest, max_shortest = 300, 250
+        max_longest, max_shortest = 960, 720
 
         if (width >= height and (width > max_longest or height > max_shortest)) or (height > width and (height > max_longest or width > max_shortest)):
             if width > height:
@@ -88,6 +125,20 @@ class Person(NewsItem):
 
             new_name = self.image.name.split('.')[0] + '.jpg'
             self.image.save(new_name, img_file)
+
+    def hash_image(self, block_size=65536):
+        hasher = hashlib.md5()
+        filename = MEDIA_ROOT + '/' + self.image.name
+
+        with open(filename, 'rb') as f:
+            for buf in iter(partial(f.read, block_size), b''):
+                hasher.update(buf)
+
+            if not self.image_hash or self.image_hash != hasher.hexdigest().lower():
+                self._image_hash = hasher.digest()
+                self.image.name = 'people/' + hasher.hexdigest().lower() + '.jpg'
+                new_filename = MEDIA_ROOT + '/' + self.image.name
+                os.rename(filename, new_filename)
 
     def render(self, request):
         return render(request, 'home/person_home.html', {
@@ -115,6 +166,10 @@ class Person(NewsItem):
     @property
     def image_hash(self):
         return str(b16encode(self._image_hash).lower(), 'utf-8') if self._image_hash else None
+
+    @property
+    def thumbnail_hash(self):
+        return str(b16encode(self._thumbnail_hash).lower(), 'utf-8') if self._thumbnail_hash else None
 
     class Meta:
         verbose_name_plural = 'people'
